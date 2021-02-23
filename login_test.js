87f433522eec36333993d55749120ac325f22de4
schego1ev <schegolev.s.n@gmail.com>
2021-02-23 15:49:07 +0300

    added negative test

--- login_test.js
+++ login_test.js
@@ -2,10 +2,20 @@ Feature('login');
 
 const { I } = inject();
 
-Scenario('API POST', async () => {
+Scenario('API POST positive', async () => {
     const res = await I.sendPostRequest('api/user/login', { "email": "mnemonik@internet.ru", "password": "k9@v285wVR@E43W" });
     I.assertEqual(res.status, 200);
     console.log('Response : ', res);
-    console.log('Response status : ', res.status)
-    console.log('Response Data: ', res.data)
+    // console.log('Response status : ', res.status)
+    // console.log('Response Data: ', res.data)
+
+});
+
+Scenario('API POST negative', async () => {
+    const resNegativ = await I.sendPostRequest('api/user/login', { "email": "mnemonik@internet.ru", "password": "*k9@v285wVR@E43W" });
+    I.assertEqual(resNegativ.status, 403);
+    console.log('Response : ', resNegativ);
+    // console.log('Response status : ', res.status)
+    // console.log('Response Data: ', res.data)
+
 });
